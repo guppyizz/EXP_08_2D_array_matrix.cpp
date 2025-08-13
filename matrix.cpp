54c2a2b8cce406eb6bcbbc2ec9f606108756cd95
Updesh arora <updesh.arora4200@gmail.com>
2025-08-13 20:24:16 +0530

    Create matrix.cpp

---
+++ matrix.cpp
@@ -0,0 +1,34 @@
+// UPDESH ARORA
+// PRN - 24070123124
+// ENTC B-3
+// EXPERIMENT 06
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m,n;
+    cout<<"Enter the number of rows: ";
+    cin>>m;
+    cout<<"Enter the number of columns: ";
+    cin>>n;
+    int mat[m][n];
+    for (int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<"Enter the elements of the matrix: ";
+            cin>>mat[i][j];
+        }
+    }
+    for (int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+          cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+
+}
